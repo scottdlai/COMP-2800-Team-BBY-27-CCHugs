f63b0598eac02c55856811d41fa5be7f07aa5373
Sean Wallace <swallace987@gmail.com>
2020-05-01 14:48:03 -0700

    Bugs fixed, and routing complete

--- CCHugs-project/src/App.svelte
+++ CCHugs-project/src/App.svelte
@@ -2,17 +2,15 @@
 	import router from "page"
 	import signup from './signup.svelte'
 	import login from './login.svelte'
+	import Chat from "./chat/Chat.svelte";
 
     let page;
     let params;
-	import Chat from "./chat/Chat.svelte";
-	export let name;
-</script>
 
-<Chat />
 
 	router('/signup',() => page = signup);
 	router('/login',() => page = login);
+	router('/chat', () => page = Chat);
 
     router.start();
 </script>
