0718779c6d92124c1458f2a5ce611e0bb65b0533
Victor Cho <victorcho@outlook.com>
2020-05-05 15:38:44 -0700

    Added Buttons and some functionality to MainPage.

--- CCHugs-project/src/MainPage.svelte
+++ CCHugs-project/src/MainPage.svelte
@@ -1,40 +1,135 @@
 <script>
-	let mainProfile = 'Profile';
-	let mainChat = 'Self Awareness & Mental Help';
-	let mainSendAHug = 'Send A Hug';
-	let mainCheckHug = 'Check Hugs';
-	let mainFiendsList = 'Fiends List';
-	let mainChats = 'Chats';
-	let mainFeedback = 'Feedback';
-	let name = 'Victor'
-	const handleProfile = function () {
-		name = 'orange';
-	};
+	let name = '.....'
+		let button = [
+		{buttonName: 'Profile', handle: function () {
+		name = 'Profile'}},
+
+		{buttonName:'Self Awareness & Mental Help', handle: function () {
+		name = "Self Awareness & Mental help"}},
+
+		{buttonName:'Send A Hug', handle: function () {
+		name = "Send A Hug"}},
+
+				{buttonName: 'Check Hugs', handle: function () {
+		name = "Check Hugs"}},
+
+						{buttonName:'Fiends List', handle: function () {
+		name = "Friends List"}},
+
+								{buttonName:'Chats', handle: function () {
+		name = "Chats"}},
+
+										{buttonName:'Feedback', handle: function () {
+		name = "Feedback"}},
+	];
+	console.log(button);
 </script>
 
 <main>
-	<h1>My name is {name}!</h1>
-	<button on:click = {handleProfile}>{mainProfile}</button>
+	<header>
+	<h1>Going to {name}!</h1>
+	<hr>
+	</header>
+
+	<section>
+		<div class="buttonDisplay">
+		{#each button as button}
+		<button on:click={button.handle}>{button.buttonName}</button>
+		{/each}
+		</div>
+	</section>
+
+	<footer>
+	<h1>This is a footer</h1>
+	</footer>
 </main>
 
 <style>
 	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+		display: grid;
+		grid-template-areas:
+		"header"
+		"section"
+		"footer";
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+	header {
+		grid-area: header;
 	}
 
-	@media (min-width: 640px) {
+	section {
+		grid-area: section;
+	}
+
+	footer {
+		grid-area: footer;
+	}
+
+		@media (min-width: 1024px) {
 		main {
-			max-width: none;
+		grid-template-columns: repeat(3, 1fr);
+		grid-template-areas:
+		"header header header"
+		"section section section"
+		"footer footer footer";
+		}
+
+		.buttonDisplay {
+			display:grid;
+			grid-gap: 50px 50px;
+			grid-template-columns:repeat(3, 1fr);
+			grid-template-areas:
+			"section section section"
+		}
+
+	}
+
+	@media (min-width: 440px) and (max-width: 1024px) {
+		main {
+			grid-template-columns: repeat(2, 1fr);
+			grid-template-areas:
+		"header header"
+		"section section"
+		"footer footer";
+		}
+
+				.buttonDisplay {
+			display:grid;
+			grid-gap: 50px 50px;
+			grid-template-columns: repeat(2, 1fr);
+			grid-template-areas:
+			"section section"
 		}
 	}
+
+	@media (max-width: 440px) {
+		main {
+			grid-template-columns: repeat(1, 1fr);
+			grid-template-areas:
+		"header"
+		"section"
+		"footer";
+		}
+						.buttonDisplay {
+			display:grid;
+			grid-gap: 50px 50px;
+			grid-template-columns: repeat(1, 1fr);
+			grid-template-areas:
+			"section"
+		}
+	}
+
+		button {
+		width: 100%;
+		height: 250px;
+		background-color: #ff9e6d;
+		}
+		
+		h1 {
+		text-align: center;
+		color: #ff3e00;
+		text-transform: capitalize;
+		font-size: 4em;
+		font-weight: 100;
+		}
 </style>
