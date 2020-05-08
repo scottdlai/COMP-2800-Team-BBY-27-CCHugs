e2c60c0be6e930f317cf786eda52baca4ddcbc6f
Victor Cho <victorcho@outlook.com>
2020-05-07 17:30:03 -0700

    Fixed the app.svelte file.

--- CCHugs-project/src/App.svelte
+++ CCHugs-project/src/App.svelte
@@ -5,18 +5,10 @@
 	import login from './signup-login/login.svelte'
 	import Chat from "./chat/Chat.svelte"
 	import landingPage from './landingPage/landingPage.svelte'
-<<<<<<< HEAD
 	import userprofile from "./UserProfile/userprofile.svelte";
-<<<<<<< HEAD
-	import resources from './resources/resources.svelte'
-=======
 	import userprofileEdit from "./UserProfile/userprofile-edit.svelte";
->>>>>>> userprofile
-=======
-	import userprofile from "./UserProfile/userprofile.svelte"
-	import resources from './resources/resources.svelte'
+	import resources from './resources/resources.svelte';
 	import aboutUs from './aboutUs/aboutUs.svelte';
->>>>>>> e61e6c27ece043a8d6a0020f6ea7907e8b00f252
 
     let page;
     let params;
@@ -27,15 +19,9 @@
 	router('/chat', () => page = Chat);
 	router('/landing', () => page = landingPage);
 	router('/userprofile', () => page = userprofile);
-<<<<<<< HEAD
 	router('/resources', () => page = resources);
-<<<<<<< HEAD
-=======
 	router('/userprofileEdit', () => page = userprofileEdit);
->>>>>>> userprofile
-=======
 	router('/aboutus', () => page = aboutUs);
->>>>>>> e61e6c27ece043a8d6a0020f6ea7907e8b00f252
     router.start();
 </script>
 
