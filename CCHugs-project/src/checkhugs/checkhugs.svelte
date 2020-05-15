3e31c2c86c1ff59b65226c97eb3fe53d662b3d9c
April Tsang <tsang.apr@gmail.com>
2020-05-15 00:16:35 -0700

    check hugs for logged in user

--- CCHugs-project/src/checkhugs/checkhugs.svelte
+++ CCHugs-project/src/checkhugs/checkhugs.svelte
@@ -2,58 +2,70 @@
 import Navbar from '../components/Navbar.svelte';
 import Footer from '../components/Footer.svelte';
 import { auth, firestore } from '../Firebase.js';
+import { authState } from 'rxfire/auth';
+import { collectionData } from 'rxfire/firestore';
+
+export let uid;
+
 
 let src = "../DifferentLogo2.svg";
 
-let count = 0;
+let count = 0; 
 
-//create element and render hugs
-function renderHug(doc){
+let hugPromise = getHugs();
+// let userPromise = getUser();
 
-    let hugList = document.getElementById('hugs-list');
 
-    let div = document.createElement('div')
-    let li = document.createElement('li');
-    let author = document.createElement('span');
-    let content = document.createElement('span');
-    let time = document.createElement('span');
+async function getHugs() {
+    let snapshot = await firestore.collection('Hugs').where("receiver", "==", uid)
+        .get();
+        console.log(snapshot.docs);
+
+        return snapshot.docs.map(doc => doc.data());
     
-    let sheet = document.createElement('style')
-    sheet.innerHTML = "li { margin: 15px 0px; padding: 10px; background-image: linear-gradient(135deg, #6DFFE7, #ffffff); border-radius: 25px; border: 2px solid black;}";
-    document.body.appendChild(sheet);
-
-    div.classList.add('backgroundContainer');
-
-    li.setAttribute('data-id', doc.id);
-    author = doc.data().author;
-    content = doc.data().content;
-    time = doc.data().time.toDate();
-
-    let author1 = document.createTextNode(`<b>From: </b>`  + author);
-    let content1 = document.createTextNode("Message: " + content);
-    let time1 = document.createTextNode("Time: " + time);
-
-    li.appendChild(author1);
-    li.appendChild(document.createElement('br'));
-    li.appendChild(content1);
-    li.appendChild(document.createElement('br'));
-    li.appendChild(time1);
-    li.appendChild(document.createElement('br'));
-    div.appendChild(li)
-    hugList.appendChild(div);
 }
 
-firestore.collection('Hugs').get().then((snapshot) => {
-    snapshot.docs.forEach(doc => {
-        renderHug(doc);
-        count += 1;
-    })
-})
+
+// async function getHugs() {
+//     let snapshot = await firestore.collection('Hugs').where("receiver", "==", uid)
+//         .get();
+//         console.log(snapshot.docs);
+
+//         return snapshot.docs.map(doc => doc.data());
+    
+// }
+
+
+
+//   async function getUser() {
+//     const query = firestore.collection('Users').doc();
+
+//     const userDoc = await query.get();
+
+//     return {
+//       username: userDoc.get('username'),
+//       id: userDoc.id
+//     };
+//   }
+
+function showProfile() {
+  auth.onAuthStateChanged(function (user) {
+		  firestore.collection("Users").doc(user.uid)
+		  .onSnapshot(function (snap) {
+			  let userName = snap.data().displayName;
+			  document.getElementById("dname").innerHTML= userName;
+
+		  })
+  });
+}
+showProfile(); 
+ 
+
 </script>
 
 
-<style>
 
+<style>
 header {
     overflow: hidden;
     text-align: center;
@@ -62,24 +74,27 @@ header {
     top: 0;
     width: 100%;
 }
-
 #logo {
     width: 80%;
 }
-
 main {
     margin: 0% 20%;
 }
-
 h1 {
     margin: 10px;
     padding: 10px;
     text-align: center;
 }
-
 ul {
     list-style: none;
     padding: 0px;
+}
+p { 
+    margin: 15px 0px; 
+    padding: 10px; 
+    background-image: linear-gradient(135deg, #6DFFE7, #ffffff); 
+    border-radius: 25px; 
+    border: 2px solid black;
 }
 
   /** tablet*/
@@ -100,18 +115,39 @@ ul {
 </style>
 
 <header>
-    <img {src} alt=logo id="logo">
+    <img {src} alt="logo" id="logo">
 </header>
 
 <Navbar />
-
+          
 <main>
     <h1>Hugs Received</h1>
     <h3>You've recieved <b>{count}</b> {count === 1 ? 'hug' : 'hugs'}!</h3>
-    <div>
-        <ul id="hugs-list"></ul>
+
+
+<!-- {#await userPromise then user} -->
+    {#if uid}
+    <h6><span id="dname"></span> logged in!</h6>
+    {/if}
+<!-- {/await} -->
+
+
+
+
+
+        <ul id="hugs-list">
+            {#await hugPromise then hugReceived}
+                {#each hugReceived as hugs}
+                <p> Hug Recieved!<br>
+                <b>From: </b>{hugs.author}<br>
+                <b>Message: </b>{hugs.content}<br>
+                <b>Time: </b>{hugs.time.toDate()} 
+                </p>
+                {/each}
+            {/await}               
+        </ul>
     <div>  
 
-</main>
+</main> 
 
 <Footer />
